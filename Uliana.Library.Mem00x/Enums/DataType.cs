2d05dc75df411b31a01391da0cdc79f68ea89708
Jonas (0x000027) Uliana <dev.uliana@gmail.com>
2018-05-05 22:36:04 +0000

    adicionada estruturas de tipos válidos para escrita na memória

---
+++ Uliana.Library.Mem00x/Enums/DataType.cs
@@ -0,0 +1,24 @@
+﻿namespace Uliana.Library.Mem00x.Enums
+{
+    /// <summary>
+    /// Enumera os tipos válidos para escrita na memória.
+    /// </summary>
+    public enum DataType
+    {
+        Float,
+
+        Int,
+
+        Byte,
+
+        Bytes,
+
+        DByte,
+
+        Double,
+
+        Long,
+
+        String
+    }
+}
